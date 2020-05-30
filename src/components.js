bdb748333ba9501654880a431809a1198abea37e
chemicstry <chemicstry@gmail.com>
2020-05-30 14:02:25 +0300

    Add ecsy example

---
+++ src/components.js
@@ -0,0 +1,53 @@
+import { Vector2 } from "./math.js";
+
+export class Movement {
+    constructor() {
+        this.velocity = new Vector2();
+        this.acceleration = new Vector2();
+    }
+
+    reset() {
+        this.velocity.set(0, 0);
+        this.acceleration.set(0, 0);
+    }
+}
+
+export class Circle {
+    constructor() {
+        this.position = new Vector2();
+        this.radius = 0;
+        this.velocity = new Vector2();
+        this.acceleration = new Vector2();
+    }
+
+    reset() {
+        this.position.set(0, 0);
+        this.radius = 0;
+        this.velocity.set(0, 0);
+        this.acceleration.set(0, 0);
+    }
+}
+
+export class CanvasContext {
+    constructor() {
+        this.ctx = null;
+        this.width = 0;
+        this.height = 0;
+    }
+}
+
+export class DemoSettings {
+    constructor() {
+        this.speedMultiplier = 0.001;
+    }
+}
+
+export class Intersecting {
+    constructor() {
+        this.points = [];
+    }
+
+    reset() {
+        this.points.length = 0;
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,1 +1,62 @@
-console.log("Hello")
+import {World, System} from 'ecsy';
+import {Movement, Circle, CanvasContext, DemoSettings, Intersecting} from './components.js';
+import {MovementSystem, Renderer, IntersectionSystem} from './systems.js';
+import {random} from './utils.js';
+
+var world = new World();
+
+world
+    .registerComponent(Circle)
+    .registerComponent(Movement)
+    .registerComponent(Intersecting)
+    .registerSystem(MovementSystem)
+    .registerSystem(Renderer)
+    .registerSystem(IntersectionSystem);
+
+// Used for singleton components
+var singletonEntity = world.createEntity()
+    .addComponent(CanvasContext)
+    .addComponent(DemoSettings);
+
+var canvas = document.querySelector("canvas");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+var canvasComponent = singletonEntity.getMutableComponent(CanvasContext);
+canvasComponent.ctx = canvas.getContext("2d");
+canvasComponent.width = canvas.width;
+canvasComponent.height = canvas.height;
+
+for (var i = 0; i < 30; i++) {
+    var entity = world
+        .createEntity()
+        .addComponent(Circle)
+        .addComponent(Movement);
+
+    var circle = entity.getMutableComponent(Circle);
+    circle.position.set(random(0, canvas.width), random(0, canvas.height), 0);
+    circle.radius = random(20, 100);
+
+    var movement = entity.getMutableComponent(Movement);
+    movement.velocity.set(random(-20, 20), random(-20, 20), 0);
+}
+
+window.world = world;
+window.Circle = Circle;
+window.Movement = Movement;
+
+window.addEventListener( 'resize', () => {
+    canvasComponent.width = canvas.width = window.innerWidth
+    canvasComponent.height = canvas.height = window.innerHeight;
+}, false );
+
+var lastTime = performance.now();
+function update() {
+    var time = performance.now();
+    var delta = time - lastTime;
+    lastTime = time;
+    world.execute(delta);
+    requestAnimationFrame(update);
+}
+
+update();
